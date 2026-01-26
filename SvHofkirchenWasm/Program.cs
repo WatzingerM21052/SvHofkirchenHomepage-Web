98938f3e63b9ce808576f91bfd6637dbbca6a31e
Matthias <matthias.watzinger1@gmail.com>
2026-01-26 22:02:43 +0100

    Youth Juhu

--- SvHofkirchenWasm/Program.cs
+++ SvHofkirchenWasm/Program.cs
@@ -10,7 +10,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 // WICHTIG: Hier registrieren wir unseren DataService
-builder.Services.AddScoped<DataService>();
 builder.Services.AddScoped<YouthService>();
 
 await builder.Build().RunAsync();
